e37eb3dbeb10c114a5623627cdeed37eac718e39
Leonardo Jaques <jaques.projetos@outlook.com>
2022-12-13 10:41:37 -0300

    feat: add apolo clientt

--- desafio-react/src/index.tsx
+++ desafio-react/src/index.tsx
@@ -1,15 +1,19 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { client } from './service/Api';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
